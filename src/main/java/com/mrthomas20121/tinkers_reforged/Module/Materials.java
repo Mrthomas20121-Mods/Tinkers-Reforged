a9ea388b2cbc7c267500794bd86c7d5357bfa719
mrthomas20121 <thomas77300@gmail.com>
2020-04-20 07:55:25 +0200

    Materials

---
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Module/Materials.java
@@ -0,0 +1,24 @@
+package com.mrthomas20121.tinkers_reforged.Module;
+
+import slimeknights.tconstruct.library.materials.Material;
+
+import java.util.ArrayList;
+
+public class Materials {
+
+    public static ArrayList<Material> mats = new ArrayList<Material>();
+    // thermalfoundation Materials
+    public static final Material enderium = new Material("reforged_enderium", 0x006C5F);
+    public static final Material signalum = new Material("reforged_signalum", 0xA32500);
+    public static final Material lumium = new Material("reforged_lumium", 0xEAD981);
+
+    // naturesaura Materials
+    public static final Material infused_iron = new Material("reforged_infused_iron", 0x34BA3D);
+
+    // Applied Energistic 2
+    public static final Material certus_quartz = new Material("reforged_certus_quartz", 0xBADBFF);
+    public static final Material fluix_crystal = new Material("reforged_fluix_crystal", 0x614EAB);
+
+    // astralsorcery
+    public static final Material starmetal = new Material("reforged_starmetal", 0x001169);
+}
---
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Module/Modules.java
@@ -0,0 +1,12 @@
+package com.mrthomas20121.tinkers_reforged.Module;
+
+public class Modules {
+
+    // base module
+    public static ModuleBase module = new ModuleBase();
+
+    public static final ModuleThermal thermal = new ModuleThermal();
+    public static final ModuleNatureAura natureAura = new ModuleNatureAura();
+    public static final ModuleAe ae = new ModuleAe();
+    public static final ModuleAstralSorcery astral = new ModuleAstralSorcery();
+}
