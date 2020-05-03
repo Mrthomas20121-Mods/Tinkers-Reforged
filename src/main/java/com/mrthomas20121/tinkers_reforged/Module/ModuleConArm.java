8b52476b64b64a1892b659f52a263e1fa0f342fa
mrthomas20121 <thomas77300@gmail.com>
2020-05-03 11:59:58 +0200

    More mods support for construct armory

--- src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleConArm.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleConArm.java
@@ -16,16 +16,19 @@ public class ModuleConArm extends ModuleBase {
 
     public static boolean conarm = Loader.isModLoaded("conarm");
 
-    public ModuleConArm() { }
+    public ModuleConArm() {
+
+    }
 
     public void preInit(FMLPreInitializationEvent e) {
         if(Loader.isModLoaded("thermal") && Config.thermal) {
             if(Config.enderium) {
+                ArmorRegistryLib enderium = new ArmorRegistryLib(Materials.enderium);
+                enderium.setCoreStats(19.5f, 25.6f);
+                enderium.setPlatesStats(0.9f, 8, 0.5f);
+                enderium.setTrimStats(30);
+                enderium.registerArmor();
                 ArmorMaterials.addArmorTrait(Materials.enderium, Traits.ender);
-                TinkerRegistry.addMaterialStats(Materials.enderium,
-                        new CoreMaterialStats(300, 30),
-                        new PlatesMaterialStats(1, 90, 2f),
-                        new TrimMaterialStats(50.5f));
             }
             if(Config.lumium) {
                 ArmorMaterials.addArmorTrait(Materials.lumium, Traits.light);
@@ -35,6 +38,7 @@ public class ModuleConArm extends ModuleBase {
                         new TrimMaterialStats(50f));
             }
             if(Config.signalum) {
+
                 ArmorMaterials.addArmorTrait(Materials.signalum, Traits.signal);
                 TinkerRegistry.addMaterialStats(Materials.signalum,
                         new CoreMaterialStats(250, 29),
@@ -44,99 +48,113 @@ public class ModuleConArm extends ModuleBase {
         }
         if(Loader.isModLoaded("appliedenergistics2") && Config.ae2) {
             if(Config.certus_quartz) {
-                ArmorMaterials.addArmorTrait(Materials.certus_quartz, ArmorTraits.skeletal);
-                ArmorMaterials.addArmorTrait(Materials.certus_quartz, ArmorTraits.calcic, ArmorMaterialType.CORE);
-                TinkerRegistry.addMaterialStats(Materials.certus_quartz,
-                        new CoreMaterialStats(12, 15),
-                        new PlatesMaterialStats(0.85f, 5, 0f),
-                        new TrimMaterialStats(3.5f));
+                ArmorRegistryLib certus_quartz = new ArmorRegistryLib(Materials.fluix_crystal);
+                certus_quartz.registerMaterialTrait(ArmorTraits.skeletal);
+                certus_quartz.registerMaterialTrait(ArmorTraits.calcic, ArmorMaterialType.CORE);
+                certus_quartz.setCoreStats(12, 15);
+                certus_quartz.setPlatesStats(0.85f, 5, 0f);
+                certus_quartz.setTrimStats(3.5f);
+                certus_quartz.registerArmor();
             }
             if(Config.fluix_crystal) {
-                ArmorMaterials.addArmorTrait(Materials.fluix_crystal, Traits.signal);
-                ArmorMaterials.addArmorTrait(Materials.fluix_crystal, ArmorTraits.skeletal, ArmorMaterialType.CORE);
-                TinkerRegistry.addMaterialStats(Materials.fluix_crystal,
-                        new CoreMaterialStats(12, 15),
-                        new PlatesMaterialStats(0.85f, 5, 0f),
-                        new TrimMaterialStats(3.5f));
+                ArmorRegistryLib fluix = new ArmorRegistryLib(Materials.fluix_crystal);
+                fluix.registerMaterialTrait(ArmorTraits.skeletal);
+                fluix.setCoreStats(12, 15);
+                fluix.setPlatesStats(0.85f, 5, 0f);
+                fluix.setTrimStats(3.5f);
+                fluix.registerArmor();
             }
         }
 
         if(Loader.isModLoaded("naturesaura") && Config.naturesaura) {
             if(Config.infused_iron) {
-                ArmorMaterials.addArmorTrait(Materials.infused_iron, Traits.AURA_INFUSION);
-                ArmorMaterials.addArmorTrait(Materials.infused_iron, ArmorTraits.magnetic2);
-                TinkerRegistry.addMaterialStats(Materials.infused_iron,
-                        new CoreMaterialStats(12, 15),
-                        new PlatesMaterialStats(0.85f, 5, 0f),
-                        new TrimMaterialStats(3.5f));
+                ArmorRegistryLib infused_iron = new ArmorRegistryLib(Materials.infused_iron);
+                infused_iron.registerMaterialTrait(Traits.AURA_INFUSION);
+                infused_iron.registerMaterialTrait(ArmorTraits.magnetic2, ArmorMaterialType.CORE);
+                infused_iron.setCoreStats(12, 15);
+                infused_iron.setPlatesStats(0.85f, 5, 0f);
+                infused_iron.setTrimStats(3.5f);
+                infused_iron.registerArmor();
             }
             if(Config.ancient_wood) {
-                ArmorMaterials.addArmorTrait(Materials.ancient_wood, Traits.AURA_INFUSION);
-                ArmorMaterials.addArmorTrait(Materials.ancient_wood, ArmorTraits.ecological);
-                TinkerRegistry.addMaterialStats(Materials.ancient_wood,
-                        new CoreMaterialStats(50, 3),
-                        new PlatesMaterialStats(1f, 4, 0f),
-                        new TrimMaterialStats(0.5f));
+                ArmorRegistryLib ancient_wood = new ArmorRegistryLib(Materials.ancient_wood);
+                ancient_wood.registerMaterialTrait(Traits.AURA_INFUSION);
+                ancient_wood.setCoreStats(50, 3);
+                ancient_wood.setPlatesStats(1f, 4, 0f);
+                ancient_wood.setTrimStats(0.5f);
+                ancient_wood.registerArmor();
             }
         }
         if(Loader.isModLoaded("atum") && Config.atum) {
             if(Config.limestone) {
-                ArmorMaterials.addArmorTrait(Materials.limestone, ArmorTraits.cheap);
-                ArmorMaterials.addArmorTrait(Materials.limestone, ArmorTraits.cheapskate);
-                TinkerRegistry.addMaterialStats(Materials.limestone,
-                        new CoreMaterialStats(9f, 5),
-                        new PlatesMaterialStats(1f, 20, 0.5f),
-                        new TrimMaterialStats(1f));
+                ArmorRegistryLib limestone = new ArmorRegistryLib(Materials.limestone);
+                limestone.registerMaterialTrait(ArmorTraits.cheap);
+                limestone.registerMaterialTrait(ArmorTraits.cheapskate);
+                limestone.setCoreStats(9f, 5);
+                limestone.setPlatesStats(1f, 20, 0.5f);
+                limestone.setTrimStats(1f);
+                limestone.registerArmor();
             }
             if(Config.khnumite) {
-                ArmorMaterials.addArmorTrait(Materials.khnumite, ArmorTraits.cheap);
-                ArmorMaterials.addArmorTrait(Materials.khnumite, ArmorTraits.cheapskate, ArmorMaterialType.PLATES);
-                TinkerRegistry.addMaterialStats(Materials.khnumite,
-                        new CoreMaterialStats(5.3f, 3),
-                        new PlatesMaterialStats(0.75f, -1.5f, 0f),
-                        new TrimMaterialStats(1.75f));
+                ArmorRegistryLib khnumite = new ArmorRegistryLib(Materials.khnumite);
+                khnumite.registerMaterialTrait(ArmorTraits.cheap);
+                khnumite.registerMaterialTrait(ArmorTraits.cheapskate, ArmorMaterialType.PLATES);
+                khnumite.setCoreStats(5.3f, 3);
+                khnumite.setPlatesStats(0.75f, -1.5f, 0f);
+                khnumite.setTrimStats(1.75f);
+                khnumite.registerArmor();
             }
         }
         if((Loader.isModLoaded("astralsorcery") && Config.astral)) {
-            ArmorMaterials.addArmorTrait(Materials.starmetal, Traits.astral);
-            ArmorMaterials.addArmorTrait(Materials.starmetal, ArmorTraits.alien, ArmorMaterialType.PLATES);
-            TinkerRegistry.addMaterialStats(Materials.starmetal,
-                    new CoreMaterialStats(12, 15),
-                    new PlatesMaterialStats(0.85f, 5, 0f),
-                    new TrimMaterialStats(3.5f));
+
+            ArmorRegistryLib starmetal = new ArmorRegistryLib(Materials.starmetal);
+            starmetal.registerMaterialTrait(Traits.astral);
+            starmetal.registerMaterialTrait(ArmorTraits.alien, ArmorMaterialType.PLATES);
+            starmetal.setCoreStats(12, 15);
+            starmetal.setTrimStats(3.5f);
+            starmetal.setPlatesStats(0.85f, 5, 0f);
+            starmetal.registerArmor();
         }
 
         if(Loader.isModLoaded("mysticalworld") && Config.mysticalworld) {
-            ArmorRegistryLib amethyst = new ArmorRegistryLib(Materials.amethyst);
-            amethyst.registerMaterialTrait(ArmorTraits.calcic);
-            amethyst.setCoreStats(95, 23);
-            amethyst.setPlatesStats(0.9f, 85, 1f);
-            amethyst.setTrimStats(45);
-            amethyst.registerArmor();
+            if(Config.amethyst) {
+                ArmorRegistryLib amethyst = new ArmorRegistryLib(Materials.amethyst);
+                amethyst.registerMaterialTrait(ArmorTraits.calcic);
+                amethyst.setCoreStats(95, 23);
+                amethyst.setPlatesStats(0.9f, 85, 1f);
+                amethyst.setTrimStats(45);
+                amethyst.registerArmor();
+            }
         }
 
         if(Loader.isModLoaded("roots") && Config.roots) {
-            ArmorRegistryLib runestone = new ArmorRegistryLib(Materials.runestone);
-            runestone.registerMaterialTrait(ArmorTraits.cheap);
-            runestone.setCoreStats(80, 15);
-            runestone.setPlatesStats(1, 80, 0f);
-            runestone.setTrimStats(45);
-            runestone.registerArmor();
+            if(Config.runestone) {
+                ArmorRegistryLib runestone = new ArmorRegistryLib(Materials.runestone);
+                runestone.registerMaterialTrait(ArmorTraits.cheap);
+                runestone.setCoreStats(80, 15);
+                runestone.setPlatesStats(1, 80, 0f);
+                runestone.setTrimStats(45);
+                runestone.registerArmor();
+            }
         }
         if(Loader.isModLoaded("bigreactors") && Config.extreme) {
-            ArmorRegistryLib yellorium = new ArmorRegistryLib(Materials.yellorium);
-            yellorium.registerMaterialTrait(Traits.radioactive);
-            yellorium.setCoreStats(95, 20);
-            yellorium.setPlatesStats(1, 85, 1f);
-            yellorium.setTrimStats(45);
-            yellorium.registerArmor();
+            if(Config.yellorium) {
+                ArmorRegistryLib yellorium = new ArmorRegistryLib(Materials.yellorium);
+                yellorium.registerMaterialTrait(Traits.radioactive);
+                yellorium.setCoreStats(95, 20);
+                yellorium.setPlatesStats(1, 85, 1f);
+                yellorium.setTrimStats(45);
+                yellorium.registerArmor();
+            }
 
-            ArmorRegistryLib blutonium = new ArmorRegistryLib(Materials.blutonium);
-            blutonium.registerMaterialTrait(Traits.nuclearwaste);
-            blutonium.setCoreStats(95, 20);
-            blutonium.setPlatesStats(1, 85, 1f);
-            blutonium.setTrimStats(45);
-            blutonium.registerArmor();
+            if(Config.blutonium) {
+                ArmorRegistryLib blutonium = new ArmorRegistryLib(Materials.blutonium);
+                blutonium.registerMaterialTrait(Traits.nuclearwaste);
+                blutonium.setCoreStats(95, 20);
+                blutonium.setPlatesStats(1, 85, 1f);
+                blutonium.setTrimStats(45);
+                blutonium.registerArmor();
+            }
         }
     }
     public void init(FMLInitializationEvent e) {
