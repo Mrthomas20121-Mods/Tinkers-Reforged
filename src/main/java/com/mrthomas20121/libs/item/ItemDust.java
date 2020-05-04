20e22e01a0b88b3a76ff52dcee3f4365b55608c3
mrthomas20121 <thomas77300@gmail.com>
2020-05-04 08:51:15 +0200

    Qivium and Lavium

--- src/main/java/com/mrthomas20121/libs/item/ItemDust.java
+++ src/main/java/com/mrthomas20121/libs/item/ItemDust.java
@@ -1,10 +1,12 @@
 package com.mrthomas20121.libs.item;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class ItemDust extends Item {
     public ItemDust(String modid, String name) {
         this.setRegistryName(modid, name+"_dust");
         this.setTranslationKey(modid+ "." + name+"_dust");
+        setCreativeTab(CreativeTabs.MISC);
     }
 }
--- src/main/java/com/mrthomas20121/libs/item/ItemIngot.java
+++ src/main/java/com/mrthomas20121/libs/item/ItemIngot.java
@@ -1,10 +1,12 @@
 package com.mrthomas20121.libs.item;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class ItemIngot extends Item {
     public ItemIngot(String modid, String name) {
         this.setRegistryName(modid, name+"_ingot");
         this.setTranslationKey(modid+ "." + name+"_ingot");
+        setCreativeTab(CreativeTabs.MISC);
     }
 }
