8109d2a270f9ff6852e0a63f80d51c08aa1185e7
mrthomas20121 <thomas77300@gmail.com>
2020-05-08 12:12:34 +0200

    all Modules now use Registry lib

--- src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleMysticalWorld.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleMysticalWorld.java
@@ -1,5 +1,6 @@
 package com.mrthomas20121.tinkers_reforged.Module;
 
+import com.mrthomas20121.libs.RegistryLib;
 import com.mrthomas20121.tinkers_reforged.Config.Config;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -13,23 +14,22 @@ import slimeknights.tconstruct.tools.TinkerTraits;
 
 public class ModuleMysticalWorld extends ModuleBase {
 
-    public ModuleMysticalWorld() {
-        Materials.mats.add(Materials.amethyst);
-    }
+    public RegistryLib amethyst = new RegistryLib(Materials.amethyst);
 
+    public ModuleMysticalWorld() {
+        amethyst.setCraftable(true).setCastable(false);
+        amethyst.registerMaterialTrait(TinkerTraits.jagged, MaterialTypes.HEAD);
+        amethyst.registerMaterialTrait(TinkerTraits.sharp);
+        amethyst.registerHeadStats(300, 6f, 5f, HarvestLevels.DIAMOND);
+        amethyst.registerHandleStats(1, 200);
+        amethyst.registerExtraStats(30);
+    }
     @Override
     public void preInit(FMLPreInitializationEvent e) {
         if(Config.amethyst) {
-            TinkerRegistry.addMaterialStats(Materials.amethyst,
-                    new HeadMaterialStats(300, 6f, 5f, HarvestLevels.COBALT),
-                    new HandleMaterialStats(1f, 200),
-                    new ExtraMaterialStats(30));
-            Materials.amethyst.addItem("gemAmethyst", 1, Material.VALUE_Gem);
-            Materials.amethyst.setCraftable(true).setCastable(false);
-            Materials.amethyst.addTrait(TinkerTraits.jagged, MaterialTypes.HEAD);
-            Materials.amethyst.addTrait(TinkerTraits.sharp);
-            MaterialIntegration amethystmi = new MaterialIntegration(Materials.amethyst).setRepresentativeItem("gemAmethyst");
-            TinkerRegistry.integrate(amethystmi).preInit();
+            amethyst.addGemItem("Amethyst");
+            amethyst.registerPreInit("gemAmethyst");
+            Materials.mats.add(amethyst.getMat());
         }
     }
     @Override
--- src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleThermal.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleThermal.java
@@ -21,139 +21,137 @@ import slimeknights.tconstruct.library.utils.HarvestLevels;
 import slimeknights.tconstruct.tools.TinkerTraits;
 
 public class ModuleThermal extends ModuleBase {
-    private String lumium = "Lumium";
-    private String enderium = "Enderium";
-    private String signalum = "Signalum";
-    private String plat = "Platinum";
-    private String iri = "Iridium";
-    private String invarStr = "Invar";
-    private String alu = "Aluminum";
+    private String lumiumOre = "Lumium";
+    private String enderiumOre = "Enderium";
+    private String signalumOre = "Signalum";
+    private String platinumOre = "Platinum";
+    private String iridiumOre = "Iridium";
+    private String invarOre = "Invar";
+    private String aluminumOre = "Aluminum";
     public RegistryLib platinum = new RegistryLib(Materials.platinum);
     public RegistryLib iridium = new RegistryLib(Materials.iridium);
     public RegistryLib invar = new RegistryLib(Materials.invar);
     public RegistryLib aluminum = new RegistryLib(Materials.aluminum);
+    public RegistryLib enderium = new RegistryLib(Materials.enderium);
+    public RegistryLib signalum = new RegistryLib(Materials.signalum);
+    public RegistryLib lumium = new RegistryLib(Materials.lumium);
 
     public ModuleThermal() {
-        Materials.mats.add(Materials.lumium);
-        Materials.mats.add(Materials.enderium);
-        Materials.mats.add(Materials.signalum);
+
+        enderium.setCraftable(false).setCastable(true);
+        enderium.registerMaterialTrait(Traits.enderfestation);
+        enderium.registerMaterialTrait(Traits.ender, MaterialTypes.HEAD);
+        enderium.registerHeadStats(750, 9.0f, 8.5f, HarvestLevels.COBALT);
+        enderium.registerHandleStats(0.9f, 170);
+        enderium.registerExtraStats(90);
+        enderium.registerBowStats(2, 1.5f, 7);
+
+        signalum.setCraftable(false).setCastable(true);
+        signalum.registerMaterialTrait(Traits.signal);
+        signalum.registerHeadStats(540, 7.3f, 6.9f, HarvestLevels.OBSIDIAN);
+        signalum.registerHandleStats(1.3f, 90);
+        signalum.registerExtraStats(80);
+        signalum.registerBowStats(4.5f, 1, 6);
+
+        lumium.setCraftable(false).setCastable(true);
+        lumium.registerMaterialTrait(Traits.light);
+        lumium.registerHeadStats(470, 7f, 5.1f, HarvestLevels.OBSIDIAN);
+        lumium.registerHandleStats(1.2f, 90);
+        lumium.registerExtraStats(80);
+        lumium.registerBowStats(2, 1, 9);
 
         platinum.setCraftable(false).setCastable(true);
         platinum.registerMaterialTrait(TinkerTraits.dense);
         platinum.registerHeadStats(500, 7.0f, 6.5f, HarvestLevels.OBSIDIAN);
         platinum.registerHandleStats(0.9f, 90);
         platinum.registerExtraStats(10);
+        platinum.registerBowStats(2, 1.5f, 7);
 
         iridium.setCastable(true).setCraftable(false);
         iridium.registerHeadStats(500, 7.1f, 6.4f, HarvestLevels.OBSIDIAN);
         iridium.registerHandleStats(1f, 90);
         iridium.registerExtraStats(15);
         iridium.registerMaterialTrait(TinkerTraits.dense);
+        iridium.registerBowStats(2, 1.5f, 7);
 
         invar.setCastable(true).setCraftable(false);
         invar.registerHeadStats(500, 5.7f, 6.3f, HarvestLevels.OBSIDIAN);
         invar.registerHandleStats(1f, 90);
         invar.registerExtraStats(15);
         invar.registerMaterialTrait(TinkerTraits.hellish);
+        invar.registerBowStats(2, 1.5f, 7);
 
         aluminum.setCastable(true).setCraftable(false);
         aluminum.registerHeadStats(450, 6.1f, 6f, HarvestLevels.OBSIDIAN);
         aluminum.registerHandleStats(1f, 70);
         aluminum.registerExtraStats(30);
         aluminum.registerMaterialTrait(TinkerTraits.dense);
+        aluminum.registerBowStats(2, 1.5f, 7);
 
-        Materials.mats.add(platinum.getMat());
-        Materials.mats.add(iridium.getMat());
-        Materials.mats.add(invar.getMat());
-        Materials.mats.add(aluminum.getMat());
     }
     @Override
     public void preInit(FMLPreInitializationEvent e) {
-
         if(Config.enderium) {
-            Fluid enderiumF = FluidRegistry.getFluid((enderium.toLowerCase()));
-            TinkerRegistry.addMaterialStats(Materials.enderium,
-                    new HeadMaterialStats(750, 9.0f, 8.5f, HarvestLevels.COBALT),
-                    new HandleMaterialStats(0.9f, 170),
-                    new ExtraMaterialStats(100));
-            Materials.enderium.addCommonItems(enderium);
-            Materials.enderium.setFluid(enderiumF);
-            Materials.enderium.addTrait(Traits.enderfestation, MaterialTypes.HANDLE);
-            Materials.enderium.addTrait(Traits.enderfestation, MaterialTypes.HEAD);
-            Materials.enderium.addTrait(Traits.ender, MaterialTypes.HEAD);
-            Materials.enderium.addTrait(Traits.enderfestation, MaterialTypes.EXTRA);
-            Materials.enderium.setCraftable(false).setCastable(true);
-            MaterialIntegration enderiummi = new MaterialIntegration(Materials.enderium, enderiumF).setRepresentativeItem("ingot" + enderium);
-            TinkerRegistry.integrate(enderiummi).toolforge().preInit();
+            Fluid fluid = FluidRegistry.getFluid((enderiumOre.toLowerCase()));
+            enderium.registerPreInit(enderiumOre, fluid);
+            Materials.mats.add(enderium.getMat());
         }
-
         if(Config.signalum) {
-            Fluid signalumF = FluidRegistry.getFluid((signalum.toLowerCase()));
-            TinkerRegistry.addMaterialStats(Materials.signalum,
-                    new HeadMaterialStats(540, 7.1f, 5f, HarvestLevels.OBSIDIAN),
-                    new HandleMaterialStats(1.3f, 90),
-                    new ExtraMaterialStats(70));
-            Materials.signalum.setCraftable(false).setCastable(true);
-            Materials.signalum.addCommonItems(signalum);
-            Materials.signalum.setFluid(signalumF);
-            Materials.signalum.addTrait(Traits.signal, MaterialTypes.HANDLE);
-            Materials.signalum.addTrait(Traits.signal, MaterialTypes.HEAD);
-            Materials.signalum.addTrait(Traits.signal, MaterialTypes.EXTRA);
-            MaterialIntegration signalumlmi = new MaterialIntegration(Materials.signalum, signalumF).setRepresentativeItem("ingot" + signalum);
-            TinkerRegistry.integrate(signalumlmi).toolforge().preInit();
+            Fluid fluid = FluidRegistry.getFluid((signalumOre.toLowerCase()));
+            signalum.registerPreInit(signalumOre, fluid);
+            Materials.mats.add(signalum.getMat());
         }
-
         if(Config.lumium) {
-            Fluid lumiumF = FluidRegistry.getFluid((lumium.toLowerCase()));
-            TinkerRegistry.addMaterialStats(Materials.lumium,
-                    new HeadMaterialStats(470, 7f, 5.1f, HarvestLevels.OBSIDIAN),
-                    new HandleMaterialStats(1.2f, 90),
-                    new ExtraMaterialStats(80));
-            Materials.lumium.setCraftable(false).setCastable(true);
-            Materials.lumium.addCommonItems(lumium);
-            Materials.lumium.setFluid(lumiumF);
-            Materials.lumium.addTrait(TinkerTraits.aridiculous, MaterialTypes.HANDLE);
-            Materials.lumium.addTrait(Traits.light, MaterialTypes.HEAD);
-            Materials.lumium.addTrait(Traits.light, MaterialTypes.EXTRA);
-            MaterialIntegration lumiummi = new MaterialIntegration(Materials.lumium, lumiumF).setRepresentativeItem("ingot" + lumium);
-            TinkerRegistry.integrate(lumiummi).toolforge().preInit();
+            Fluid fluid = FluidRegistry.getFluid((lumiumOre.toLowerCase()));
+            lumium.registerPreInit(lumiumOre, fluid);
+            Materials.mats.add(lumium.getMat());
         }
         if(Config.platinum) {
-            platinum.addCommonItems(plat);
-            platinum.registerPreInit("Platinum", FluidRegistry.getFluid("platinum"));
+            platinum.addCommonItems(platinumOre);
+            Fluid fluid = FluidRegistry.getFluid((platinumOre.toLowerCase()));
+            platinum.registerPreInit(platinumOre, fluid);
+            Materials.mats.add(platinum.getMat());
         }
         if(Config.iridium) {
-            iridium.addCommonItems(iri);
-            iridium.registerPreInit(iri, FluidRegistry.getFluid(iri.toLowerCase()));
+            iridium.addCommonItems(iridiumOre);
+            Fluid fluid = FluidRegistry.getFluid((iridiumOre.toLowerCase()));
+            iridium.registerPreInit(iridiumOre, fluid);
+            Materials.mats.add(iridium.getMat());
         }
         if(Config.invar) {
-            invar.addCommonItems(invarStr);
-            invar.registerPreInit(invarStr, FluidRegistry.getFluid(invarStr.toLowerCase()));
+            invar.addCommonItems(invarOre);
+            Fluid fluid = FluidRegistry.getFluid((invarOre.toLowerCase()));
+            invar.registerPreInit(invarOre, fluid);
+            Materials.mats.add(invar.getMat());
         }
         if(Config.aluminum) {
-            aluminum.addCommonItems(alu);
-            aluminum.registerPreInit(alu, FluidRegistry.getFluid(alu.toLowerCase()));
+            aluminum.addCommonItems(aluminumOre);
+            Fluid fluid = FluidRegistry.getFluid((aluminumOre.toLowerCase()));
+            aluminum.registerPreInit(aluminumOre, fluid);
+            Materials.mats.add(aluminum.getMat());
         }
     }
     @Override
     public void init(FMLInitializationEvent e) {
         if(Config.enderium) {
-            Materials.enderium.setFluid(FluidRegistry.getFluid((enderium.toLowerCase())));
-            Materials.enderium.setCastable(true).setCraftable(false);
+            enderium.registerInitFluid(FluidRegistry.getFluid((enderiumOre.toLowerCase())), enderiumOre);
+            enderium.setCastable(true).setCraftable(false);
         }
-
         if(Config.signalum) {
-            Materials.signalum.setFluid(FluidRegistry.getFluid((signalum.toLowerCase())));
-            Materials.signalum.setCastable(true).setCraftable(false);
+            signalum.registerInitFluid(FluidRegistry.getFluid((signalumOre.toLowerCase())), signalumOre);
+            signalum.setCastable(true).setCraftable(false);
         }
-
         if(Config.lumium) {
-            Materials.lumium.setFluid(FluidRegistry.getFluid((lumium.toLowerCase())));
-            Materials.lumium.setCastable(true).setCraftable(false);
+            lumium.registerInitFluid(FluidRegistry.getFluid((lumiumOre.toLowerCase())), lumiumOre);
+            lumium.setCastable(true).setCraftable(false);
         }
         if(Config.iridium) {
-            iridium.registerInitFluid(FluidRegistry.getFluid((iri.toLowerCase())), iri);
+            iridium.registerInitFluid(FluidRegistry.getFluid((iridiumOre.toLowerCase())), iridiumOre);
             iridium.setCraftable(false).setCastable(true);
+        }
+        if(Config.platinum) {
+            platinum.registerInitFluid(FluidRegistry.getFluid((platinumOre.toLowerCase())), platinumOre);
+            platinum.setCraftable(false).setCastable(true);
         }
     }
     @Override
