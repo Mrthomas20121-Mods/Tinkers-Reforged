8fa80f8d76e95fd0b26bb727ca53ff809ac4fa32
mrthomas20121 <thomas77300@gmail.com>
2020-05-08 12:15:03 +0200

    Bumped version to 1.1.6

--- src/main/java/com/mrthomas20121/tinkers_reforged/TinkersReforged.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/TinkersReforged.java
@@ -27,7 +27,7 @@ public class TinkersReforged
     public static TinkersReforged instance;
     public static final String MODID = "tinkers_reforged";
     public static final String NAME = "Tinkers' Reforged";
-    public static final String VERSION = "1.1.3";
+    public static final String VERSION = "1.1.6";
 
     public static Logger logger;
 
