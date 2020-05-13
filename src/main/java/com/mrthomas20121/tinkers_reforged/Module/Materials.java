8a7c0e28708c5a7c5e1a1bc1c80137bc84b591e3
mrthomas20121 <thomas77300@gmail.com>
2020-05-13 20:07:06 +0200

    Added aa mats and changed the materials order

--- src/main/java/com/mrthomas20121/tinkers_reforged/Module/Materials.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Module/Materials.java
@@ -7,19 +7,15 @@ import java.util.ArrayList;
 public class Materials {
 
     public static ArrayList<Material> mats = new ArrayList<>();
-    // thermalfoundation Materials
-    public static final Material enderium = new Material("reforged_enderium", 0x006C5F);
-    public static final Material signalum = new Material("reforged_signalum", 0xA32500);
-    public static final Material lumium = new Material("reforged_lumium", 0xEAD981);
-    public static final Material platinum = new Material("reforged_platinum", 0x85D1F2);
-    public static final Material invar = new Material("reforged_invar", 0xA8AFAB);
-    public static final Material iridium = new Material("reforged_iridium", 0xDBDAEF);
-    public static final Material aluminum = new Material("reforged_aluminum", 0xC4C6D3);
 
-    // naturesaura Materials
-    public static final Material infused_iron = new Material("reforged_infused_iron", 0x34BA3D);
-    public static final Material ancient_wood = new Material("reforged_ancient_wood", 0xA06686);
-    public static  final Material ingot_of_the_sky = new Material("reforged_sky", 0x9BDFFF);
+    // actually addition
+    public static final Material blackquartz = new Material("reforged_blackquartz", 0x161515);
+    public static final Material void_crystal = new Material("reforged_void", 0x171717);
+    public static final Material enori = new Material("reforged_enori", 0xEBE3FF);
+    public static final Material redstonia = new Material("reforged_redstonia", 0xF30000);
+    public static final Material emeradic = new Material("reforged_emeradic", 0x15EA0A);
+    public static final Material diamantine = new Material("reforged_diamantine", 0x9A9CFF);
+    public static final Material palis = new Material("reforged_palis", 0x9A9CFF);
 
     // Applied Energistic 2
     public static final Material certus_quartz = new Material("reforged_certus_quartz", 0xBADBFF);
@@ -32,11 +28,13 @@ public class Materials {
     public static final Material khnumite = new Material("reforged_khnumite", 0x998B59);
     public static final Material limestone = new Material("reforged_limestone", 0x9F8D7A);
 
-    // mystical world
-    public static final Material amethyst = new Material("reforged_amethyst", 0x832CAD);
-
-    // roots
-    public static final Material runestone = new Material("reforged_runestone", 0x575761);
+    // blueskies
+    public static final Material ventium = new Material("reforged_ventium", 0xC31A1F);
+    public static final Material horizonite = new Material("reforged_horizonite", 0xF27B2B);
+    public static final Material pyrope = new Material("reforged_pyrope", 0xDA283E);
+    public static final Material turquoise = new Material("reforged_turquoise", 0x5AE7D1);
+    public static final Material charoite = new Material("reforged_charoite", 0xB78CDF);
+    public static final Material diopside = new Material("reforged_diopside", 0x4CE849);
 
     // extreme reactor
     public static final Material yellorium = new Material("reforged_yellorium", 0xD9DB5C);
@@ -50,13 +48,25 @@ public class Materials {
     public static final Material refined_glowstone = new Material("reforged_glowstone", 0xEAC829);
     public static final Material hdpe_sheet = new Material("reforged_hdpesheet", 0xF9F9F4);
 
-    // blueskies
-    public static final Material ventium = new Material("reforged_ventium", 0xC31A1F);
-    public static final Material horizonite = new Material("reforged_horizonite", 0xF27B2B);
-    public static final Material pyrope = new Material("reforged_pyrope", 0xDA283E);
-    public static final Material turquoise = new Material("reforged_turquoise", 0x5AE7D1);
-    public static final Material charoite = new Material("reforged_charoite", 0xB78CDF);
-    public static final Material diopside = new Material("reforged_diopside", 0x4CE849);
+    // mystical world
+    public static final Material amethyst = new Material("reforged_amethyst", 0x832CAD);
+
+    // naturesaura Materials
+    public static final Material infused_iron = new Material("reforged_infused_iron", 0x34BA3D);
+    public static final Material ancient_wood = new Material("reforged_ancient_wood", 0xA06686);
+    public static final Material ingot_of_the_sky = new Material("reforged_sky", 0x9BDFFF);
+
+    // roots
+    public static final Material runestone = new Material("reforged_runestone", 0x575761);
+
+    // thermalfoundation Materials
+    public static final Material enderium = new Material("reforged_enderium", 0x006C5F);
+    public static final Material signalum = new Material("reforged_signalum", 0xA32500);
+    public static final Material lumium = new Material("reforged_lumium", 0xEAD981);
+    public static final Material platinum = new Material("reforged_platinum", 0x85D1F2);
+    public static final Material invar = new Material("reforged_invar", 0xA8AFAB);
+    public static final Material iridium = new Material("reforged_iridium", 0xDBDAEF);
+    public static final Material aluminum = new Material("reforged_aluminum", 0xC4C6D3);
 
     // tinkers'reforged
     public static final Material lavium = new Material("reforged_lavium", 0x9FAF59);
