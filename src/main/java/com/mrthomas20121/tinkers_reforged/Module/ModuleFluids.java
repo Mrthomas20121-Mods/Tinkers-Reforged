7eaacce1a2497f43d6e04139cd3890ca56f60d1a
mrthomas20121 <thomas77300@gmail.com>
2020-05-16 21:38:27 +0200

    Fluid Registry! (broken)

--- src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleFluids.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleFluids.java
@@ -1,20 +1,126 @@
 package com.mrthomas20121.tinkers_reforged.Module;
 
-import slimeknights.tconstruct.library.fluid.FluidMolten;
+import java.util.ArrayList;
 
+import com.mrthomas20121.libs.Fluid.FluidMoltenBase;
+import com.mrthomas20121.libs.block.BlockFluid;
+import com.mrthomas20121.tinkers_reforged.TinkersReforged;
+import com.mrthomas20121.tinkers_reforged.Config.Config;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
+
+@Mod.EventBusSubscriber(modid = TinkersReforged.MODID)
 public class ModuleFluids {
-    public static FluidMolten lavium = new FluidMolten("lavium", Materials.lavium.materialTextColor);
-    public static FluidMolten qivium = new FluidMolten("qivium", Materials.qivium.materialTextColor);
-    public static FluidMolten kovar = new FluidMolten("kovar", Materials.kovar.materialTextColor);
-    public static FluidMolten blutonium = new FluidMolten("blutonium", Materials.blutonium.materialTextColor);
-    public static FluidMolten infused_iron = new FluidMolten("infused_iron", Materials.infused_iron.materialTextColor);
-    public static FluidMolten ludicrite = new FluidMolten("ludicrite", Materials.ludicrite.materialTextColor);
-    public static FluidMolten molten_of_the_sky = new FluidMolten("molten_of_the_sky", Materials.ingot_of_the_sky.materialTextColor);
-    public static FluidMolten starmetal = new FluidMolten("starmetal", Materials.starmetal.materialTextColor);
-    public static FluidMolten refined_obsidian = new FluidMolten("refined_obsidian", Materials.refined_obsidian.materialTextColor);
-    public static FluidMolten refined_glowstone = new FluidMolten("refined_glowstone", Materials.refined_glowstone.materialTextColor);
-    public static FluidMolten osmium = new FluidMolten("osmium", Materials.osmium.materialTextColor);
-    public static FluidMolten ventium = new FluidMolten("ventium", Materials.ventium.materialTextColor);
-    public static FluidMolten horizonite = new FluidMolten("horizonite", Materials.horizonite.materialTextColor);
-    public static FluidMolten pyrope = new FluidMolten("pyrope", Materials.pyrope.materialTextColor);
+    public static ArrayList<BlockFluid> blockFluids = new ArrayList<>(); 
+
+    public static FluidMoltenBase lavium = new FluidMoltenBase("lavium", Materials.lavium.materialTextColor);
+    public static FluidMoltenBase qivium = new FluidMoltenBase("qivium", Materials.qivium.materialTextColor);
+    public static FluidMoltenBase kovar = new FluidMoltenBase("kovar", Materials.kovar.materialTextColor);
+    public static FluidMoltenBase ludicrite = new FluidMoltenBase("ludicrite", Materials.ludicrite.materialTextColor);
+    public static FluidMoltenBase blutonium = new FluidMoltenBase("blutonium", Materials.blutonium.materialTextColor);
+    public static FluidMoltenBase infused_iron = new FluidMoltenBase("infused_iron", Materials.infused_iron.materialTextColor);
+    public static FluidMoltenBase molten_of_the_sky = new FluidMoltenBase("molten_of_the_sky", Materials.ingot_of_the_sky.materialTextColor);
+    public static FluidMoltenBase starmetal = new FluidMoltenBase("starmetal", Materials.starmetal.materialTextColor);
+    public static FluidMoltenBase refined_obsidian = new FluidMoltenBase("refined_obsidian", Materials.refined_obsidian.materialTextColor);
+    public static FluidMoltenBase refined_glowstone = new FluidMoltenBase("refined_glowstone", Materials.refined_glowstone.materialTextColor);
+    public static FluidMoltenBase osmium = new FluidMoltenBase("osmium", Materials.osmium.materialTextColor);
+    public static FluidMoltenBase ventium = new FluidMoltenBase("ventium", Materials.ventium.materialTextColor);
+    public static FluidMoltenBase horizonite = new FluidMoltenBase("horizonite", Materials.horizonite.materialTextColor);
+    public static FluidMoltenBase pyrope = new FluidMoltenBase("pyrope", Materials.pyrope.materialTextColor);
+    public static FluidMoltenBase blackquartz = new FluidMoltenBase("black_quartz", Materials.blackquartz.materialTextColor);
+    public static FluidMoltenBase enori = new FluidMoltenBase("enori", Materials.enori.materialTextColor);
+
+    public static void preInit(FMLPreInitializationEvent e) {
+        if(Config.qivium) {
+            FluidRegistry.registerFluid(qivium);
+            FluidRegistry.addBucketForFluid(qivium);
+        }
+        if(Config.lavium) {
+            FluidRegistry.registerFluid(lavium);
+            FluidRegistry.addBucketForFluid(lavium);
+        }
+        if(Config.kovar) {
+            kovar.setDensity(8);
+            kovar.setTemperature(750);
+            FluidRegistry.registerFluid(kovar);
+            FluidRegistry.addBucketForFluid(kovar);
+        }
+    }
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
+        IForgeRegistry<Block> r = event.getRegistry();
+
+        if(Config.qivium) {
+            RegisterFluidBlock(r, qivium);
+        }
+        if(Config.lavium) {
+            RegisterFluidBlock(r, lavium);
+        }
+        if(Config.kovar) {
+            RegisterFluidBlock(r, kovar);
+        }
+        if(Loader.isModLoaded("bigreactors")) {
+            if(Config.ludicrite) {
+                RegisterFluidBlock(r, ludicrite);
+            }
+            if(Config.blutonium) {
+                RegisterFluidBlock(r, blutonium);
+            }
+        }
+        if(Loader.isModLoaded("naturesaura")) {
+            if(Config.infused_iron) {
+                RegisterFluidBlock(r, infused_iron);
+            }
+            if(Config.ingot_of_the_sky) {
+                RegisterFluidBlock(r, molten_of_the_sky);
+            }
+        }
+        if(Config.starmetal && Loader.isModLoaded("astralsorcery"))  {
+            RegisterFluidBlock(r, starmetal);
+        }
+        if(Loader.isModLoaded("mekanism")) {
+            if(Config.osmium) {
+                RegisterFluidBlock(r, osmium);
+            }
+            if(Config.refined_obsidian) {
+                RegisterFluidBlock(r, refined_obsidian);
+            }
+            if(Config.refined_glowstone) {
+                RegisterFluidBlock(r, refined_glowstone);
+            }
+        }
+        if(Loader.isModLoaded("blue_skies")) {
+            if(Config.ventium) {
+                RegisterFluidBlock(r, ventium);
+            }
+            if(Config.pyrope) {
+                RegisterFluidBlock(r, pyrope);
+            }
+            if(Config.horizonite) {
+                RegisterFluidBlock(r, horizonite);
+            }
+        }
+        if(Loader.isModLoaded("actuallyadditions")) {
+            if(Config.blackquartz) {
+                RegisterFluidBlock(r, blackquartz);
+            }
+            if(Config.enori) {
+                RegisterFluidBlock(r, enori);
+            }
+        }
+    }
+    protected static void RegisterFluidBlock(IForgeRegistry<Block> r, Fluid f) {
+        BlockFluid fluidBlock = new BlockFluid(f);
+        fluidBlock.setRegistryName(TinkersReforged.MODID, "molten_"+f.getName());
+        blockFluids.add(fluidBlock);
+        r.register(fluidBlock);
+    }
 }
