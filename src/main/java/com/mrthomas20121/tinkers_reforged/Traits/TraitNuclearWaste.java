6ab0396e8bb563e5b1f7d94e8e5b5deeb005c468
mrthomas20121 <thomas77300@gmail.com>
2020-05-17 10:52:24 +0200

    Reworked NuclearWaste and Radioactive Traits.

--- src/main/java/com/mrthomas20121/tinkers_reforged/Traits/TraitNuclearWaste.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Traits/TraitNuclearWaste.java
@@ -1,40 +1,66 @@
 package com.mrthomas20121.tinkers_reforged.Traits;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
-import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.living.*;
-import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
-import net.minecraftforge.fml.common.Optional;
-import slimeknights.tconstruct.library.modifiers.ModifierNBT;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
 
 public class TraitNuclearWaste extends AbstractTrait {
+
+    private Potion[] effects = {
+        MobEffects.ABSORPTION, 
+        MobEffects.BLINDNESS, 
+        MobEffects.FIRE_RESISTANCE, 
+        MobEffects.GLOWING, 
+        MobEffects.HASTE, 
+        MobEffects.HEALTH_BOOST, 
+        MobEffects.HUNGER,
+        MobEffects.INVISIBILITY,
+        MobEffects.JUMP_BOOST,
+        MobEffects.LUCK,
+        MobEffects.MINING_FATIGUE,
+        MobEffects.NAUSEA,
+        MobEffects.NIGHT_VISION,
+        MobEffects.POISON,
+        MobEffects.REGENERATION,
+        MobEffects.RESISTANCE,
+        MobEffects.SLOWNESS,
+        MobEffects.SPEED,
+        MobEffects.STRENGTH,
+        MobEffects.UNLUCK,
+        MobEffects.WATER_BREATHING,
+        MobEffects.WEAKNESS,
+        MobEffects.WITHER
+    };
+
     public TraitNuclearWaste() {
         super("nuclear_waste", 0xFFFFFF);
     }
-    @Override
-    public void onUpdate(ItemStack tool, World world, Entity entity, int itemSlot, boolean isSelected) {
-    }
 
     @Override
-    public int onToolDamage(ItemStack tool, int damage, int newDamage, EntityLivingBase entity) {
-        if(entity.isEntityAlive()) {
-            if(entity instanceof EntityPlayer) {
-
-                if(this.isToolWithTrait(entity.getHeldItemMainhand())) {
-                    entity.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 100, 1));
-                    entity.addPotionEffect(new PotionEffect(MobEffects.HUNGER, 100, 1));
-                    entity.addPotionEffect(new PotionEffect(MobEffects.POISON, 100, 0));
-                }
+    public void afterBlockBreak(ItemStack tool, World world, IBlockState state, BlockPos pos, EntityLivingBase player, boolean wasEffective) {
+        if(wasEffective) {
+            if(!world.isRemote) {
+                int random_effect = world.rand.nextInt(22);
+                int effect_duration = world.rand.nextInt(50)*10;
+                int amplitude = world.rand.nextInt(4);
+                player.addPotionEffect(new PotionEffect(effects[random_effect], effect_duration, amplitude));
             }
+        }
+    }
+    @Override
+    public float damage(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
+        if(!player.world.isRemote) {
+            int random_effect = player.world.rand.nextInt(22);
+            int effect_duration = player.world.rand.nextInt(50)*10;
+            int amplitude = player.world.rand.nextInt(4);
+            player.addPotionEffect(new PotionEffect(effects[random_effect], effect_duration, amplitude));
         }
         return newDamage;
     }
--- src/main/java/com/mrthomas20121/tinkers_reforged/Traits/TraitRadioactive.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Traits/TraitRadioactive.java
@@ -1,17 +1,14 @@
 package com.mrthomas20121.tinkers_reforged.Traits;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
-
-import java.util.List;
 
 public class TraitRadioactive extends AbstractTrait {
     public TraitRadioactive() {
@@ -21,15 +18,36 @@ public class TraitRadioactive extends AbstractTrait {
     @Override
     public void onUpdate(ItemStack tool, World world, Entity entity, int itemSlot, boolean isSelected) {
         if(entity.isEntityAlive()) {
-            if(entity instanceof EntityPlayer) {
-                if(this.isToolWithTrait(tool) && isSelected) {
-                    List<Entity> entities = world.getLoadedEntityList();
-                    for(Entity ent : entities) {
-                        if(ent instanceof EntityLivingBase && !(ent instanceof EntityPlayer)) {
-                            ((EntityLivingBase) ent).addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 100, 1));
-                            ((EntityLivingBase) ent).addPotionEffect(new PotionEffect(MobEffects.POISON, 200, 1));
-                        }
+            if(entity instanceof EntityLivingBase) {
+                if( ((EntityLivingBase)entity).isPotionActive(MobEffects.POISON) && isSelected ) {
+                    int rand = world.rand.nextInt(100);
+                    if(rand>50) {
+                        ((EntityLivingBase)entity).removePotionEffect(MobEffects.POISON);
                     }
+                }
+                else if( ((EntityLivingBase)entity).isPotionActive(MobEffects.WITHER) && isSelected ) {
+                    int rand = world.rand.nextInt(100);
+                    if(rand>50) {
+                        ((EntityLivingBase)entity).removePotionEffect(MobEffects.WITHER); 
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void afterBlockBreak(ItemStack tool, World world, IBlockState state, BlockPos pos, EntityLivingBase player, boolean wasEffective) {
+        if(this.isToolWithTrait(tool)) {
+            int rand = world.rand.nextInt(100);
+            if(rand<10 && wasEffective) {
+                player.addPotionEffect(new PotionEffect(MobEffects.HASTE, 100, 1));
+            }
+            else if(rand>90 && wasEffective) {
+                player.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 100, 1));
+            }
+            else {
+                if(wasEffective) {
+                    player.addPotionEffect(new PotionEffect(MobEffects.NAUSEA, 100, 0));
                 }
             }
         }
