f3a73a4aee0332c2701d2ef754505bdca003471c
mrthomas20121 <thomas77300@gmail.com>
2020-05-17 16:34:06 +0200

    Fluid Blockstates

---
+++ genGenFluidsBlockstates.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+let fluids = [
+	"qivium", 
+	"ludicrite", 
+	"blutonium", 
+	"infused_iron", 
+	"molten_of_the_sky", 
+	"starmetal", 
+	"refined_obsidian",
+	"refined_glowstone",
+	"osmium",
+	"ventium",
+	"horizonite",
+	"pyrope",
+	"black_quartz",
+	"enori"
+];
+
+for(fluid of fluids) {
+let data = {
+	 "forge_marker": 1,
+	 "defaults": {
+	  "model": "forge:fluid"
+	 },
+	 "variants": {
+	  "normal": [
+			{
+			"custom": {
+				"fluid": fluid
+				}
+			}
+		]
+	}
+}
+
+fs.writeFileSync(`./src/main/resources/assets/tinkers_reforged/blockstates/molten_${fluid}.json`, JSON.stringify(data, null, 2), 'utf8')
+}
