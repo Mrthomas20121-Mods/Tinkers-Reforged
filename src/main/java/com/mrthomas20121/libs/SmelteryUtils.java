407c6f65534c5e0c5bd786e8c579defdbdfd548e
mrthomas20121 <thomas77300@gmail.com>
2020-05-17 11:23:06 +0200

    Smeltery Utils stuff

--- src/main/java/com/mrthomas20121/libs/SmelteryUtils.java
+++ src/main/java/com/mrthomas20121/libs/SmelteryUtils.java
@@ -2,16 +2,102 @@ package com.mrthomas20121.libs;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
 import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.materials.Material;
+import slimeknights.tconstruct.library.smeltery.AlloyRecipe;
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
 
 import static slimeknights.tconstruct.smeltery.TinkerSmeltery.*;
 
 public class SmelteryUtils {
-    public static void RegisterGemCasting(String ore, Fluid fluid) {
-      ItemStack result = OredictHelper.getOre("gem"+ore);
-      if(!result.equals(ItemStack.EMPTY)) {
-        TinkerRegistry.registerTableCasting(result, castGem, fluid, Material.VALUE_Gem);
-      }
+
+    public static int VALUE_Gear = Material.VALUE_Ingot*4;
+
+    public static void registerIngotCasting(String ore, Fluid fluid) {
+        ItemStack result = OredictHelper.getOre("ingot"+ore);
+        if(!result.equals(ItemStack.EMPTY)) {
+          TinkerRegistry.registerTableCasting(result, castIngot, fluid, Material.VALUE_Ingot);
+        }
+    }
+    public static void registerPlateCasting(String ore, Fluid fluid) {
+        ItemStack result = OredictHelper.getOre("plate"+ore);
+        if(!result.equals(ItemStack.EMPTY)) {
+          TinkerRegistry.registerTableCasting(result, castPlate, fluid, Material.VALUE_Ingot);
+        }
+    }
+    public static void registerGearCasting(String ore, Fluid fluid) {
+        ItemStack result = OredictHelper.getOre("gear"+ore);
+        if(!result.equals(ItemStack.EMPTY)) {
+          TinkerRegistry.registerTableCasting(result, castGear, fluid, VALUE_Gear);
+        }
+    }
+    public static void registerGemCasting(String ore, Fluid fluid) {
+        ItemStack result = OredictHelper.getOre("gem"+ore);
+        if(!result.equals(ItemStack.EMPTY)) {
+            TinkerRegistry.registerTableCasting(result, castGem, fluid, Material.VALUE_Gem);
+        }
+    }
+    public static void registerNuggetCasting(String ore, Fluid fluid) {
+        ItemStack result = OredictHelper.getOre("nugget"+ore);
+        if(!result.equals(ItemStack.EMPTY)) {
+            TinkerRegistry.registerTableCasting(result, castNugget, fluid, Material.VALUE_Nugget);
+        }
+    }
+    public static void registerBlockCasting(String ore, Fluid fluid) {
+        ItemStack result = OredictHelper.getOre("block"+ore);
+        if(!result.equals(ItemStack.EMPTY)) {
+            TinkerRegistry.registerBasinCasting(result, ItemStack.EMPTY, fluid, Material.VALUE_Block);
+        }
+    }
+    public static void registerDefaultCasting(String oredict, Fluid fluid, boolean isgem) {
+
+        String[] oredicts = { "block", "ingot", "plate", "gear", "nugget"};
+        if(isgem) oredicts[1] = "gem";
+
+        for(String ore: oredicts) {
+            if(ore=="ingot") {
+                registerIngotCasting(ore, fluid);
+            }
+            else if(ore=="block") {
+                registerBlockCasting(ore, fluid);
+            }
+            else if(ore=="plate") {
+                registerPlateCasting(ore, fluid);
+            }
+            else if(ore=="nugget") {
+                registerNuggetCasting(ore, fluid);
+            }
+            else if(ore=="gear") {
+                registerGearCasting(ore, fluid);
+            }
+            else if(ore=="gem") {
+                registerGemCasting(ore, fluid);
+            }
+        }
+    }
+    public static void registerDefaultMelting(String ore, Fluid fluid, boolean cast) {
+        TinkerRegistry.registerMelting("ingot" + ore, fluid, Material.VALUE_Ingot);
+        TinkerRegistry.registerMelting("dust" + ore, fluid, Material.VALUE_Ingot);
+        TinkerRegistry.registerMelting("plate" + ore, fluid, Material.VALUE_Ingot);
+        TinkerRegistry.registerMelting("gear" + ore, fluid, Material.VALUE_Ingot * 4);
+        TinkerRegistry.registerMelting("block" + ore, fluid, Material.VALUE_Block);
+        if (cast) {
+            registerDefaultCasting(ore, fluid, false);
+        }
+    }
+    public static void registerAlloy(FluidStack output, FluidStack... inputs) {
+        AlloyRecipe recipe = new AlloyRecipe(output, inputs);
+        TinkerRegistry.registerAlloy(recipe);
+    }
+    public static boolean checkIfAlloyRecipeExist(FluidStack output, FluidStack... inputs) {
+        boolean exist = false;
+        AlloyRecipe recipe = new AlloyRecipe(output, inputs);
+        for(AlloyRecipe alloy: TinkerRegistry.getAlloys()) {
+            if(alloy.equals(recipe)) {
+                exist = true;
+            }
+        }
+        return exist;
     }
 }
--- src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleBase.java
+++ src/main/java/com/mrthomas20121/tinkers_reforged/Module/ModuleBase.java
@@ -32,45 +32,4 @@ public abstract class ModuleBase {
         TinkerRegistry.registerTableCasting(OreDictionary.getOres("ingot" + ore).get(0), castIngot, fluid,
                 Material.VALUE_Ingot);
     }
-
-    protected void registerDefaultMelting(String ore, Fluid fluid, boolean cast) {
-        TinkerRegistry.registerMelting("ingot" + ore, fluid, Material.VALUE_Ingot);
-        TinkerRegistry.registerMelting("dust" + ore, fluid, Material.VALUE_Ingot);
-        TinkerRegistry.registerMelting("plate" + ore, fluid, Material.VALUE_Ingot);
-        TinkerRegistry.registerMelting("gear" + ore, fluid, Material.VALUE_Ingot * 4);
-        TinkerRegistry.registerMelting("block" + ore, fluid, Material.VALUE_Block);
-        if (cast) {
-            this.registerDefaultCasting(ore, fluid, false);
-        }
-    }
-
-    protected void registerDefaultCasting(String oredict, Fluid fluid, boolean isgem) {
-
-        String[] oredicts = { "block", "ingot", "plate", "gear", "nugget"};
-        if(isgem) oredicts[1] = "gem";
-
-        for(String ore: oredicts) {
-            ItemStack result = OredictHelper.getOre(ore+oredict);
-            if(!(result == ItemStack.EMPTY)) {
-                if(ore=="ingot") {
-                    TinkerRegistry.registerTableCasting(result, castIngot, fluid, Material.VALUE_Ingot);
-                }
-                else if(ore=="block") {
-                    TinkerRegistry.registerBasinCasting(result, ItemStack.EMPTY, fluid, Material.VALUE_Block);
-                }
-                else if(ore=="plate") {
-                    TinkerRegistry.registerTableCasting(result, castPlate, fluid, Material.VALUE_Ingot);
-                }
-                else if(ore=="nugget") {
-                    TinkerRegistry.registerTableCasting(result, castNugget, fluid, Material.VALUE_Nugget);
-                }
-                else if(ore=="gear") {
-                    TinkerRegistry.registerTableCasting(result, castGear, fluid, Material.VALUE_Ingot*4);
-                }
-                else if(ore=="gem") {
-                    TinkerRegistry.registerTableCasting(result, castGem, fluid, Material.VALUE_Gem);
-                }
-            }
-        }
-    }
 }
