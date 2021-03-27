80321eddc9d1d22f57b2efe82f64db2f43da03fa
mrthomas20121 <thomas77300@gmail.com>
2021-03-27 14:47:54 +0100

    Trait Fey nerf

--- src/main/java/mrthomas20121/tinkers_reforged/trait/TraitFey.java
+++ src/main/java/mrthomas20121/tinkers_reforged/trait/TraitFey.java
@@ -1,13 +1,18 @@
 package mrthomas20121.tinkers_reforged.trait;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import slimeknights.tconstruct.library.tools.ToolNBT;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
 import slimeknights.tconstruct.library.utils.HarvestLevels;
 import slimeknights.tconstruct.library.utils.TagUtil;
+import slimeknights.tconstruct.library.utils.ToolHelper;
 
+/**
+ * Fey Trait
+ * give the tool an overall stats boost but reduce the attack speed.
+ */
 public class TraitFey extends AbstractTrait {
-    private int boost = 2;
     public TraitFey()
     {
         super("ref_fey", 0x67AFAF);
@@ -16,16 +21,16 @@ public class TraitFey extends AbstractTrait {
     @Override
     public void applyEffect(NBTTagCompound rootCompound, NBTTagCompound modifierTag) {
         ToolNBT data = TagUtil.getToolStats(rootCompound);
-        data.durability += 100;
+        data.durability += 200;
 
         if(data.harvestLevel < HarvestLevels.OBSIDIAN) {
             data.harvestLevel++;
         }
 
-        data.attack += 1f;
-        data.speed += 1f;
+        data.attack += 2f;
+        data.speed += 2f;
         data.modifiers += 1;
-        data.attackSpeedMultiplier+=0.5f;
+        data.attackSpeedMultiplier*=0.5f;
 
         TagUtil.setToolTag(rootCompound, data.get());
     }
