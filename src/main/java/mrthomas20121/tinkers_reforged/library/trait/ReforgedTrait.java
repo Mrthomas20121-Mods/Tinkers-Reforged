1547c8aad5fc7150dae18743066e6127e97306a4
mrthomas20121 <thomas77300@gmail.com>
2021-07-30 10:35:07 +0200

    Renamed RightClickTrait

---
+++ src/main/java/mrthomas20121/tinkers_reforged/library/trait/ReforgedTrait.java
@@ -0,0 +1,31 @@
+package mrthomas20121.tinkers_reforged.library.trait;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import slimeknights.tconstruct.library.traits.AbstractTrait;
+
+import java.util.List;
+
+public class ReforgedTrait extends AbstractTrait {
+
+    public ReforgedTrait(String name, int color) {
+        super(name, color);
+    }
+
+    public void onTooltip(ItemStack tool, EntityPlayer player, List<String> toolTip) {}
+
+    public void onItemRightClick(ItemStack tool, World world, EntityPlayer player, BlockPos pos, EnumHand hand) {}
+
+    public void onBlockRightClick(ItemStack tool, World world, EntityPlayer player, BlockPos pos, EnumHand hand) {}
+
+    public void onEntityRightClick(ItemStack tool, World world, EntityPlayer player, BlockPos pos, Entity target, EnumHand hand) {}
+
+    public void onEntityAttack(ItemStack tool, EntityPlayer player, EntityLivingBase target, DamageSource source, float amount, LivingAttackEvent event) {}
+}
