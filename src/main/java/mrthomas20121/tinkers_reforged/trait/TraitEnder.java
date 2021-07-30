5557b8a667dfca45e74210c0bfb3db33bd021e8f
mrthomas20121 <thomas77300@gmail.com>
2021-07-30 10:37:14 +0200

    Ender Trait now also prevent enderman from teleporting

--- src/main/java/mrthomas20121/tinkers_reforged/trait/TraitEnder.java
+++ src/main/java/mrthomas20121/tinkers_reforged/trait/TraitEnder.java
@@ -1,20 +1,24 @@
 package mrthomas20121.tinkers_reforged.trait;
 
+import mrthomas20121.tinkers_reforged.library.potion.PotionRegistry;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
 
 public class TraitEnder extends AbstractTrait {
     public TraitEnder() {
         super("ref_ender", 0x376B64);
     }
+
     @Override
-    public float damage(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
-        float dmg = newDamage;
-        if(target.getName().contains("Ender")) {
-            dmg = dmg*(dmg/2);
+    public void afterHit(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damageDealt, boolean wasCritical, boolean wasHit) {
+        if(wasHit && !target.isPotionActive(PotionRegistry.ender)) {
+            target.addPotionEffect(new PotionEffect(PotionRegistry.ender, 100, 0, false, false));
         }
-        return dmg;
+        else {
+            target.removePotionEffect(PotionRegistry.ender);
+        }
     }
 }
 
