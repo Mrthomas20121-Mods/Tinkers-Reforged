348922157834768e12c786255414e6b2aef9e5c7
tong-ge <99661788@qq.com>
2022-12-19 17:10:04 +0800

    Fix some bugs

--- src/main/java/mrthomas20121/tinkers_reforged/trait/TraitTradeOff.java
+++ src/main/java/mrthomas20121/tinkers_reforged/trait/TraitTradeOff.java
@@ -4,6 +4,7 @@ import mrthomas20121.tinkers_reforged.library.trait.ReforgedTrait;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -15,7 +16,7 @@ import java.util.UUID;
 
 public class TraitTradeOff extends ReforgedTrait {
 
-    public static final AttributeModifier healthModifier = new AttributeModifier(UUID.fromString("4dd40d86-656f-4cb6-ac94-10509e7156b2"), "Tinkers Reforged Health Modifier", -10d, 0);
+    public static final AttributeModifier healthModifier = new AttributeModifier(UUID.fromString("4dd40d86-656f-4cb6-ac94-10509e7156b2"), "Tinkers Reforged Health Modifier", -0.5d, 2);
 
     public TraitTradeOff() {
         super("ref_tradeoff", 0x0);
@@ -23,11 +24,12 @@ public class TraitTradeOff extends ReforgedTrait {
 
     @Override
     public void applyEffect(NBTTagCompound rootCompound, NBTTagCompound modifierTag) {
+        super.applyEffect(rootCompound,modifierTag);
         ToolNBT data = TagUtil.getToolStats(rootCompound);
 
-        data.attackSpeedMultiplier*=2;
-        data.attack*=2;
-        data.speed*=2;
+        data.attackSpeedMultiplier *= 2;
+        data.attack *= 2;
+        data.speed *= 2;
 
         TagUtil.setToolTag(rootCompound, data.get());
     }
@@ -35,17 +37,21 @@ public class TraitTradeOff extends ReforgedTrait {
     @Override
     public void onUpdate(ItemStack tool, World world, Entity entity, int itemSlot, boolean isSelected) {
         // do stuff if selected
-        if(isSelected) {
-            if(entity instanceof EntityPlayer) {
-                EntityPlayer player = (EntityPlayer)entity;
-                if(!player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).hasModifier(healthModifier) && !player.isCreative()) {
-                    player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).applyModifier(healthModifier);
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+            IAttributeInstance i=player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
+            if (isSelected) {
+                if (!i.hasModifier(healthModifier) && !player.capabilities.isCreativeMode) {
+                    i.applyModifier(healthModifier);
+                    if(player.getHealth()>i.getAttributeValue())
+                    {
+                        player.setHealth((float)i.getAttributeValue());
+                    }
                 }
-            }
-        }
-        else {
-            if(((EntityPlayer)entity).getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).hasModifier(healthModifier)) {
-                ((EntityPlayer)entity).getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).removeModifier(healthModifier);
+            } else {
+                if (i.hasModifier(healthModifier)){
+                    i.removeModifier(healthModifier);
+                }
             }
         }
     }
