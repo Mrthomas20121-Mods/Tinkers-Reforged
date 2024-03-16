16e131f84a78aa3842efc53ab6419b146be0af05
Thomas B <thomas77300@gmail.com>
2024-03-16 16:02:20 +0100

    Update SlotMachineModifier.java

--- src/main/java/mrthomas20121/tinkers_reforged/modifier/SlotMachineModifier.java
+++ src/main/java/mrthomas20121/tinkers_reforged/modifier/SlotMachineModifier.java
@@ -1,7 +1,6 @@
 package mrthomas20121.tinkers_reforged.modifier;
 
 import mrthomas20121.tinkers_reforged.TinkersReforged;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -9,7 +8,6 @@ import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.TinkerHooks;
 import slimeknights.tconstruct.library.modifiers.hook.build.ModifierRemovalHook;
-import slimeknights.tconstruct.library.modifiers.hook.build.ToolStatsModifierHook;
 import slimeknights.tconstruct.library.modifiers.hook.build.VolatileDataModifierHook;
 import slimeknights.tconstruct.library.modifiers.util.ModifierHookMap;
 import slimeknights.tconstruct.library.tools.SlotType;
@@ -17,12 +15,10 @@ import slimeknights.tconstruct.library.tools.context.ToolRebuildContext;
 import slimeknights.tconstruct.library.tools.nbt.IModDataView;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 import slimeknights.tconstruct.library.tools.nbt.ModDataNBT;
-import slimeknights.tconstruct.library.tools.stat.ModifierStatsBuilder;
-import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
 public class SlotMachineModifier extends Modifier implements ModifierRemovalHook, VolatileDataModifierHook {
 
-    private final ResourceLocation KEY = new ResourceLocation(TinkersReforged.MOD_ID, "slot_machine");
+    private final ResourceLocation slotMachineLoc = new ResourceLocation(TinkersReforged.MOD_ID, "slot_machine");
 
     public SlotMachineModifier() {
         this.registerHooks(new ModifierHookMap.Builder()
@@ -35,13 +31,9 @@ public class SlotMachineModifier extends Modifier implements ModifierRemovalHook
     public Component onRemoved(IToolStackView tool, Modifier modifier) {
         ModDataNBT persistentData = tool.getPersistentData();
         int level = tool.getModifierLevel(this);
-        if(persistentData.contains(KEY, Tag.TAG_INT)) {
-            persistentData.remove(KEY);
-            if(level > 0) {
-                persistentData.addSlots(SlotType.ABILITY, tool.getFreeSlots(SlotType.ABILITY)-level);
-                persistentData.addSlots(SlotType.UPGRADE, tool.getFreeSlots(SlotType.UPGRADE)-level);
-            }
-        }
+        persistentData.remove(slotMachineLoc);
+        persistentData.addSlots(SlotType.ABILITY, tool.getFreeSlots(SlotType.ABILITY)-level);
+        persistentData.addSlots(SlotType.UPGRADE, tool.getFreeSlots(SlotType.UPGRADE)-level);
         return null;
     }
 
@@ -49,12 +41,10 @@ public class SlotMachineModifier extends Modifier implements ModifierRemovalHook
     public void addVolatileData(ToolRebuildContext context, ModifierEntry modifier, ModDataNBT volatileData) {
         int level = modifier.getLevel();
         IModDataView persistentData = context.getPersistentData();
-        if (!persistentData.contains(KEY, Tag.TAG_INT)) {
-            volatileData.putInt(KEY, 1);
-            if(level > 0) {
-                volatileData.addSlots(SlotType.ABILITY, level);
-                volatileData.addSlots(SlotType.UPGRADE, level);
-            }
+        if (!persistentData.contains(slotMachineLoc, Tag.TAG_BYTE)) {
+            volatileData.putBoolean(slotMachineLoc, true);
+            volatileData.addSlots(SlotType.ABILITY, level);
+            volatileData.addSlots(SlotType.UPGRADE, level);
         }
     }
 }
